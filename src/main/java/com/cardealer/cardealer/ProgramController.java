ca5a6050d0d87e286bd6189a4eea80640b8be294
Kamil Ochnik <ilmak@o2.pl>
2020-01-24 15:25:45 +0100

    initial commit

--- src/main/java/com/cardealer/cardealer/ProgramController.java
+++ src/main/java/com/cardealer/cardealer/ProgramController.java
@@ -1,11 +1,12 @@
 package com.cardealer.cardealer;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProgramController {
 
-   // @GetMapping("/")
+    @GetMapping("/")
     public String hello() {
         return "hello";
     }
